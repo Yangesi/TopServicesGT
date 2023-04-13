82d2f55f1282d7019276120590883110f783d9c5
Ortega <yangesi123@gmail.com>
2023-04-13 11:39:02 -0600

    Pruebas03_validacionEmail

--- screens/forms/postulante.jsx
+++ screens/forms/postulante.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from 'react'
 import { Modal, Form, Button } from "react-bootstrap";
 
+import { app } from '../../src/firebase/configFirebase';
+import { getAuth } from 'firebase/auth';
+import { createUserWithEmailAndPassword, fetchSignInMethodsForEmail } from 'firebase/auth';
+
 export const Postulante = ({ show, handleClose }) => {
 
-      const handleSubmit = (e) => {
-        e.preventDefault();
-        // Aquí iría el código para enviar la información del formulario al servidor
-        handleClose();
-      };
+//los estados del email
+  const [email, setEmail] = useState('');
+  const [error, setError] = useState(null);
+  const [isEmailValidated, setIsEmailValidated] = useState(false);
+
+  const auth = getAuth(app);
 
       //modulo de contrase;as, parte 1 - boton para mostrar o escoder password
     const [showPassword, setShowPassword] = useState(false);
@@ -20,32 +25,72 @@ export const Postulante = ({ show, handleClose }) => {
       setShowPassword(false); // Restablecer el estado del botón al cerrar el modal
     }, [show]);
 
-    //validacion de contrase;as, validar que sean iguales 
+   //los estados de las contrase;as
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-    const [passwordMatch, setPasswordMatch] = useState(null); // Nuevo estado
+    const [passwordMatch, setPasswordMatch] = useState(false); // Nuevo estado
     
+    //estableciendo los valores de las password
     const handlePasswordChange = (e) => {
       setPassword(e.target.value);
-      if (confirmPassword !== "" && e.target.value !== confirmPassword) {
-        setPasswordMatch(false);
-      } else {
-        setPasswordMatch(true);
-      }
     };
     
     const handleConfirmPasswordChange = (e) => {
       setConfirmPassword(e.target.value);
-      if (password !== "" && e.target.value !== password) {
-        setPasswordMatch(false);
-      } else {
-        setPasswordMatch(true);
-      }
-    };
-
+    }; 
+    
+ //validacion de contrase;as, validar que sean iguales 
     useEffect(() => {
-      setPasswordMatch(password === confirmPassword && password !== "");
+      const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&+#=(){}[\]<>|\\/^~`,.;:_-])[A-Za-z\d@$!%*?&+#=(){}[\]<>|\\/^~`,.;:_-]{8,}$/;
+      setPasswordMatch(password === confirmPassword && password !== "" && passwordRegex.test(password));
   }, [password, confirmPassword]);
+
+//validacion del email para verificar si ya existe una cuenta en firebase con ese email
+  const handleEmailValidation = async () => {
+    try {
+      const signInMethods = await fetchSignInMethodsForEmail(auth, email);
+      if (signInMethods.length > 0) {
+        setError('Este correo electrónico ya está registrado');
+        console.log('Error de handleEmailValidation, el correo ya esta registrado');
+      } else {
+        setIsEmailValidated(true);
+        setError(null);
+        console.log('todo ok con handleEmailValidation');
+      }
+    } catch (error) {
+      setError('Hubo un error al validar el correo electrónico');
+      console.log('Error de handleEmailValidation');
+    }
+  };
+
+//constante submit donde se obtienen y se envian los datos del formulario
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(email)
+    console.log(password)
+    // Aquí iría el código para enviar la información del formulario al servidor
+    //antes de registrar el usuario, verifico la validacion del email
+    if (!isEmailValidated) {
+      setError('Por favor, valide su correo electrónico antes de continuar');
+      console.log('error al validar email');
+      return;
+    }
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      setError(null);
+      console.log('Usuario registrado:', userCredential.user);
+    } catch (error) {
+      setError('Hubo un error al registrar al usuario');
+      console.log('Error al registrar el usuario')
+    }
+
+    handleClose();
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
   return (
     <>
@@ -75,9 +120,20 @@ export const Postulante = ({ show, handleClose }) => {
               <Form.Label>Correo</Form.Label>
               <Form.Control
                 type="email"
+                name="email"
                 placeholder="name@example.com"
-                
+                defaultValue={email}  
+                onChange={handleEmailChange}
+                required  
               />
+              <Form.Text>
+                {isEmailValidated === false && (
+                <span style={{ color: "red" }}>Ya existe una cuenta con esta direccion de correo electrónico</span>
+                )}
+                {isEmailValidated === true && (
+                <span style={{ color: "green" }}>Correo validado</span>
+                )}
+            </Form.Text>
             </Form.Group>
             <Form.Group className="mb-3" controlId="Password">
             <Form.Label>Contraseña</Form.Label>
@@ -85,6 +141,7 @@ export const Postulante = ({ show, handleClose }) => {
             type={showPassword ? 'text' : 'password'}
             defaultValue={password}
             onChange={handlePasswordChange}
+            name="password"
             />
             </Form.Group>
             <Form.Group className="mb-3" controlId="ConfirmarPassword">
@@ -147,15 +204,18 @@ export const Postulante = ({ show, handleClose }) => {
             <option value="empleador">Empleador</option>
           </Form.Control>
         </Form.Group>
+        <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+          <Button type="submit" variant="primary" disabled={!passwordMatch} onClick={() => { handleClose; handleEmailValidation; }}>
+            Registrarse
+          </Button>
+          <button type="button" onClick={handleEmailValidation}>
+            Validar correo electrónico
+          </button>
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Cerrar
-          </Button>
-          <Button variant="primary" disabled={!passwordMatch} onClick={handleClose}>
-            Registrarse
-          </Button>
         </Modal.Footer>
       </Modal>
     </>
--- screens/forms/validacionEmail.jsx
+++ screens/forms/validacionEmail.jsx
@@ -1,11 +1,82 @@
 import { app } from '../../src/firebase/configFirebase';
-import { getAuth, sendSignInLinkToEmail } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { useState } from 'react';
+import { createUserWithEmailAndPassword, fetchSignInMethodsForEmail, sendEmailVerification } from 'firebase/auth';
+
   
 
 const ValidacionEmail = () => {
 
   const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [isEmailValidated, setIsEmailValidated] = useState(false);
+
+  const auth = getAuth(app);
+
+  const handleEmailValidation = async () => {
+    try {
+      const signInMethods = await fetchSignInMethodsForEmail(auth, email);
+      if (signInMethods.length > 0) {
+        setError('Este correo electrónico ya está registrado');
+      } else {
+        setIsEmailValidated(true);
+        setError(null);
+      }
+    } catch (error) {
+      setError('Hubo un error al validar el correo electrónico');
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!isEmailValidated) {
+      setError('Por favor, valide su correo electrónico antes de continuar');
+      return;
+    }
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      setError(null);
+      console.log('Usuario registrado:', userCredential.user);
+    } catch (error) {
+      setError('Hubo un error al registrar al usuario');
+    }
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+    setIsEmailValidated(false);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="email">Correo electrónico:</label>
+      <input type="email" id="email" name="email" value={email} onChange={handleEmailChange} required />
+      {isEmailValidated ? (
+        <>
+          <label htmlFor="password">Contraseña:</label>
+          <input type="password" id="password" name="password" value={password} onChange={handlePasswordChange} required />
+          {error && <p>{error}</p>}
+          <button type="submit">Registrar</button>
+        </>
+      ) : (
+        <>
+          <button type="button" onClick={handleEmailValidation}>
+            Validar correo electrónico
+          </button>
+          {error && <p>{error}</p>}
+        </>
+      )}
+    </form>
+  );
+
+  /* const [email, setEmail] = useState('');
 
   const handleSignIn = (e) => {
     e.preventDefault();
@@ -38,8 +109,13 @@ const ValidacionEmail = () => {
       <input type="email" id="email" name="email" value={email} onChange={handleEmailChange} required />
       <button type="submit">Enviar correo de verificación</button>
     </form>
-  );
+  ); */
 
   };
 
 export default ValidacionEmail;
+
+
+
+
+
