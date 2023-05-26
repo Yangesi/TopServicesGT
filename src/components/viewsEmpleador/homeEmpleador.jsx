b8bd84681487d51aee446cb820760be4defd5aee
Ortega <yangesi123@gmail.com>
2023-05-25 21:48:22 -0600

    v1.31

--- src/components/viewsEmpleador/homeEmpleador.jsx
+++ src/components/viewsEmpleador/homeEmpleador.jsx
@@ -5,47 +5,105 @@ import { getServiciosPorCodigoEmpleador } from '../../../helpers/servicioEmplead
 import { TokenContext } from '../../../src/components/context/contexto';
 import { useContext } from 'react';
 
+//react bootstrap
+import { ListGroup, Button } from 'react-bootstrap';
+
 
 export const HomeEmpleador = () => {
 
-  const [datosEmpleador, setDatosEmpleador] = useState([]);
+  const [datosEmpleador, setDatosEmpleador] = useState({});
   const [datosServicios, setDatosServicios] = useState([]);
   const { token, cod_usuario, setCodigo, codigo } = useContext(TokenContext);
 
   useEffect(() => {
     if (token) {
-      //obtener los datos del empleador
+      // Obtener los datos del empleador
       const obtenerEmpleador = async () => {
         const data = await obtenerEmpleadorPorCodigo(cod_usuario, token);
         setDatosEmpleador(data);
       };
       obtenerEmpleador();
-//se establece el codigo del empleador
-      setCodigo(datosEmpleador.codigo)
-
-      //obtener los servicios seleccionados
+    } else {
+      // Redireccionar a la página de inicio de sesión
+      console.log('No existe el token');
+    }
+  }, [token]);
+  
+  useEffect(() => {
+    if (datosEmpleador.codigo) {
+      // Establecer el código del empleador
+      setCodigo(datosEmpleador.codigo);
+  
+      // Obtener los servicios seleccionados
       const obtenerServiciosEmpleador = async () => {
-        console.log(codigo)
-        console.log(token)
-        
-        const dataServicios = await getServiciosPorCodigoEmpleador(codigo, token);
+        const dataServicios = await getServiciosPorCodigoEmpleador(datosEmpleador.codigo, token);
         setDatosServicios(dataServicios);
       };
       obtenerServiciosEmpleador();
-
-      console.log('existe')
-    } else {
-      // redireccionar a la página de inicio de sesión
-      console.log('no existe')
     }
-    
-  }, [token]);
+  }, [datosEmpleador, setCodigo, token]);
 
-  //datos a renderizar en html
+    // Manejador de evento para editar un dato
+    const handleEditarDato = (dato) => {
+      // Implementa el código para editar el dato
+      console.log('Editar dato:', dato);
+    };
+  
+    // Manejador de evento para guardar cambios en un dato editado
+    const handleGuardarCambios = (dato) => {
+      // Implementa el código para guardar los cambios del dato
+      console.log('Guardar cambios:', dato);
+    };
 
-  //console.log(datosEmpleador)  
-  console.log(datosServicios)
+    console.log(datosServicios)
+
   return (
-    <div>Hola empleador</div>
-  )
-}
+    <>
+<ListGroup>
+  {Object.keys(datosEmpleador).map((key, index) => {
+    if (["apellido", "nombre", "tel", "razon_social"].includes(key)) {
+      return (
+        <ListGroup.Item key={index}>
+          <span>{datosEmpleador[key]}</span>
+          <Button variant="primary" size="sm" onClick={() => handleEditarDato(datosEmpleador[key])}>
+            Editar
+          </Button>
+          <Button variant="success" size="sm" onClick={() => handleGuardarCambios(datosEmpleador[key])}>
+            Guardar Cambios
+          </Button>
+        </ListGroup.Item>
+      );
+    }
+    return null; // Omitir otras claves del objeto
+  })}
+</ListGroup>
+ <ListGroup>
+      {datosServicios.map((servicio, index) => (
+        <ListGroup.Item key={index}>
+        <div className="fw-bold">
+          {servicio.tipo_servicio}
+        </div>
+        <div>{servicio.nombre}</div>
+        {(() => {
+          const fecha = new Date(servicio.fecha_hora);
+          const fechaLegible =
+            fecha.toLocaleDateString('es-ES') +
+            ' ' +
+            fecha.toLocaleTimeString('es-ES');
+          return <div>{fechaLegible}</div>;
+        })()}
+        {(() => {
+          const fecha = new Date(servicio.fecha_realizado);
+          const fechaLegible =
+            fecha.toLocaleDateString('es-ES') +
+            ' ' +
+            fecha.toLocaleTimeString('es-ES');
+          return <div>{fechaLegible}</div>;
+        })()}
+          {/* Puedes mostrar más propiedades del servicio según tus necesidades */}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+    </>
+  );
+};
--- src/components/viewsPostulante/homePostulante.jsx
+++ src/components/viewsPostulante/homePostulante.jsx
@@ -4,47 +4,133 @@ import { TokenContext } from '../../../src/components/context/contexto';
 import { useContext } from 'react';
 import { getServiciosPorCodigoPostulante } from '../../../helpers/servicioPostulante'
 
+//react bootstrap
+import { ListGroup, Button } from 'react-bootstrap';
+
 export const HomePostulante = () => {
 
-  const [datosPostulante, setDatosPostulante] = useState([]);
+  const [datosPostulante, setDatosPostulante] = useState({
+    apellido: '',
+    nombre: '',
+    tel: '',
+    cv: '',
+    pretencion_salarial: '',
+    comentario: '',
+  });
   const [datosServicios, setDatosServicios] = useState([]);
   const { token, cod_usuario, setCodigo, codigo } = useContext(TokenContext);
 
   useEffect(() => {
     if (token) {
-      //obtener los datos del postulante
+      // Obtener los datos del postulante
       const obtenerPostulante = async () => {
-        console.log(cod_usuario)
-        console.log(token)
+        console.log(cod_usuario);
+        console.log(token);
         const data = await getPostulantePorCodigo(cod_usuario, token);
         setDatosPostulante(data);
+        // Establecer el código del postulante
+        setCodigo(data.codigo);
       };
       obtenerPostulante();
-//se establece el codigo del postulante
-      setCodigo(datosPostulante.codigo)
-
-      //obtener los servicios seleccionados
+  
+      console.log('existe');
+    } else {
+      // Redireccionar a la página de inicio de sesión
+      console.log('no existe');
+    }
+  }, [token, cod_usuario]);
+  
+  useEffect(() => {
+    if (codigo && token) {
+      // Obtener los servicios seleccionados
       const obtenerServiciosPostulante = async () => {
-
-        
         const dataServicios = await getServiciosPorCodigoPostulante(codigo, token);
         setDatosServicios(dataServicios);
       };
-      obtenerServiciosPostulante(); 
-
-      console.log('existe')
-    } else {
-      // redireccionar a la página de inicio de sesión
-      console.log('no existe')
+      obtenerServiciosPostulante();
     }
-    
-  }, [token]);
+  }, [codigo, token]);
 
-  //console.log(datosPostulante)
+  const [editando, setEditando] = useState(false);
+
+const handleEditar = () => {
+  setEditando(true);
+};
+
+const handleGuardarCambios = () => {
+  setEditando(false);
+  // Aquí puedes realizar la lógica para guardar los cambios en el backend
+};
+
+const handleInputChange = (key, value) => {
+  setDatosPostulante((prevDatos) => ({
+    ...prevDatos,
+    [key]: value,
+  }));
+};
+  
+
+
+  console.log('datos del postulante',datosPostulante)
   console.log('datos de los servicios',datosServicios)
 
   return (
-    <div>homePostulante</div>
+    <>
+<ListGroup>
+    {Object.keys(datosPostulante).map((key, index) => {
+      if (["apellido", "nombre", "tel", "cv", "pretencion_salarial", "comentario"].includes(key)) {
+        return (
+          <ListGroup.Item key={index}>
+            <span>{key}: </span>
+            <input
+              type="text"
+              value={datosPostulante[key]}
+              onChange={(e) => handleInputChange(key, e.target.value)}
+              disabled={!editando}
+            />
+          </ListGroup.Item>
+        );
+      }
+      return null; // Omitir otras claves del objeto
+    })}
+    <Button variant="primary" size="sm" onClick={handleEditar} disabled={editando}>
+      Editar
+    </Button>
+    <Button variant="success" size="sm" onClick={handleGuardarCambios} disabled={!editando}>
+      Guardar Cambios
+    </Button>
+  </ListGroup>
+
+
+
+<ListGroup>
+  {datosServicios.map((servicio, index) => (
+    <ListGroup.Item key={index}>
+      <div className="fw-bold">
+        {servicio.tipo_servicio}
+      </div>
+      <div>{servicio.nombre}</div>
+      {(() => {
+        const fecha = new Date(servicio.fecha_hora);
+        const fechaLegible =
+          fecha.toLocaleDateString('es-ES') +
+          ' ' +
+          fecha.toLocaleTimeString('es-ES');
+        return <div>{fechaLegible}</div>;
+      })()}
+      {(() => {
+        const fecha = new Date(servicio.fecha_realizado);
+        const fechaLegible =
+          fecha.toLocaleDateString('es-ES') +
+          ' ' +
+          fecha.toLocaleTimeString('es-ES');
+        return <div>{fechaLegible}</div>;
+      })()}
+      {/* Puedes mostrar más propiedades del servicio según tus necesidades */}
+    </ListGroup.Item>
+  ))}
+</ListGroup>
+    </>
   )
 }
 
